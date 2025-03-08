40e6217b21b4d9dcc06c76873537a984d4762b11
Saul-ss0 <sansotsau004@gmail.com>
2025-03-07 19:54:24 -0600

    Update NormalIterator.java

--- src/main/java/org/equipo404/Collections/NormalIterator.java
+++ src/main/java/org/equipo404/Collections/NormalIterator.java
@@ -3,17 +3,43 @@ package org.equipo404.Collections;
 import org.equipo404.Library.Resource;
 
 import java.util.Iterator;
+/**
+ * Implementa un iterador normal para recorrer colecciones de recursos.
+ * Extiende {@link CollectionIteratorStrategy} para proporcionar una iteración estándar.
+ *
+ * @param <T> Tipo de recurso que extiende de {@link Resource}.
+ *
+ * @author Emiliano Kaleb Jimenez Rivera
+ * @author Bedoya Tellez Ariadna Valeria
+ * @author Sanchez Soto Saul
+ * @version 1.0
+ */
 
 public class NormalIterator<T extends Resource> extends CollectionIteratorStrategy<T> {
+    /**
+     * Constructor del iterador normal.
+     *
+     * @param iterator Iterador base de la colección de recursos.
+     */
 
     public NormalIterator(Iterator<T> iterator) {
         super(iterator);
     }
+    /**
+     * Verifica si hay más elementos en la colección.
+     *
+     * @return {@code true} si hay más elementos, {@code false} en caso contrario.
+     */
 
     @Override
     public boolean hasNext() {
         return this.getRawCollectionIterator().hasNext();
     }
+    /**
+     * Retorna el siguiente elemento de la colección.
+     *
+     * @return El siguiente recurso en la colección.
+     */
 
     @Override
     public T next() {
