fb7b71aeac4476a58575b8793acf6fb2daf483ce
Saul-ss0 <sansotsau004@gmail.com>
2025-03-07 20:07:37 -0600

    Update AudioBook.java

--- src/main/java/org/equipo404/Library/AudioBook.java
+++ src/main/java/org/equipo404/Library/AudioBook.java
@@ -1,5 +1,15 @@
 package org.equipo404.Library;
+/**
+ * Representa un audiolibro dentro del sistema de biblioteca digital.
+ * Extiende de la clase Resource.
+ * 
+ * @autores Emiliano Kaleb Jimenez Rivera, Bedoya Tellez Ariadna Valeria, Sanchez Soto Saul
+ * @version 1.0
+ */
 
 public class AudioBook extends Resource{
+    /**
+     * Autor del audiolibro.
+     */
     private String author;
 }
